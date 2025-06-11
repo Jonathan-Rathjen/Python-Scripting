95c54ab3193fb3d1ae26729372b2f6f0e7c74c3e
Jonathan Rathjen <jonathanrathjen@yahoo.com>
2025-06-11 09:30:53 +0000

    adding exception to find second

--- session-3/homework/find_second_largest.py
+++ session-3/homework/find_second_largest.py
@@ -1,8 +1,11 @@
 num_string = input("Enter numbers: ")
 num_list = num_string.split()
-for i in range(len(num_list)):
-    num_list[i] = int(num_list[i])
-
+try:
+    for i in range(len(num_list)):
+        num_list[i] = int(num_list[i])
+except:
+    print("One of your values is not an integer, try again")
+    pass
 largest = 0
 second = 0
 for i in num_list:
