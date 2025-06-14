7febf60a67d7f1d520acf140143e4535b481fdbb
Jonathan Rathjen <jonathanrathjen@yahoo.com>
2025-06-14 18:12:32 +0000

    adding classwork for session 5

---
+++ session-5/classwork/session-5.py
@@ -0,0 +1,106 @@
+laptop = {
+    "Dell": {
+        "software": {
+            "os": "Linux Mint",
+            "version": "22.04",
+            "applications": ["python", "vscode", "slack"]
+        },
+
+        "hardware": {
+            "model": "XPS 15",
+            "cpu": "Inter Core i5, (6 cores)",
+            "ram": "8gb",
+            "storage": "256gb",
+            "screen": "ips"
+        }
+    },
+
+    "MacBook": {
+        "software": {
+            "os": "Mac OS",
+            "version": "Sequoia 15.01",
+            "applications": ["telegram", "slack", "ms teams"]
+        },
+
+        "hardware": {
+            "model": "macbook air",
+            "cpu": "M1 (8 cores)",
+            "ram": "8gb",
+            "storage": "512gb",
+            "screen": "ips"
+        }
+    },
+
+    "Acer": {
+        "software": {
+            "os": "Linux Ubuntu",
+            "version": "22.04",
+            "applications": ["python", "nodejs", "slack"]
+        },
+
+        "hardware": {
+            "model": "Swift 16",
+            "cpu": "Inter Core i9, (16 cores)",
+            "ram": "32gb",
+            "storage": "1tb",
+            "screen": "oled"
+        }
+    }
+}
+# laptop_names = []
+# for i,j in laptop.items():
+#     laptop_names.append(i)
+# print laptop_names
+
+# for i,j in laptop.items():
+#     print("Laptop:",i)
+#     print("OS:",j['software']['os'])
+#     print("Version:",j['software']['version'])
+
+# for i,j in laptop.items():
+#     print("Laptop:",i)
+#     print("Applications: ",end = "")
+#     for k in j['software']['applications']:
+#         print(k,end = " ")
+#     print()
+
+# fruits = {"banana":2,"apple":1,"pear":3}
+# fruits["cherry"] = 4
+# fruits["pineapple"] = 7
+
+# fruits.pop('apple')
+
+# del fruits ['pear']
+
+# print(fruits)
+
+# var = {"key":"value"}
+
+# sets - mutable
+# stores only unique values
+
+# sets = {"value1","value2","value3"}
+
+# print(sets)
+
+
+# Functions
+# Snippet of code that can be repeated multiple times within the script
+# If you're using the same logic multiple times you should turn it into a function
+# return is used to output a data type
+
+# def greet(name):
+#     print("Hello,",name)
+
+# inp = input("What is your name? ")
+# greet(inp)
+
+# def add(num1,num2):
+#     return num1 + num2
+
+# print(add(5,10))
+
+
+
+
+
