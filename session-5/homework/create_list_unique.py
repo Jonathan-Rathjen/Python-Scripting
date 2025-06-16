300721e7c672c3a9a84afa062a55dcd10a1846b8
Jonathan Rathjen <jonathanrathjen@yahoo.com>
2025-06-16 20:45:39 +0000

    adding session 5 homework

---
+++ session-5/homework/create_list_unique.py
@@ -0,0 +1,67 @@
+product_data = {
+    "store": "TechNova",
+    "location": {
+        "city": "San Francisco",
+        "country": "USA"
+    },
+    "products": [
+        {
+            "id": "P1001",
+            "name": "Wireless Mouse",
+            "brand": "LogiTech",
+            "price": 29.99,
+            "currency": "USD",
+            "stock": 134,
+            "specs": {
+                "color": "Black",
+                "connectivity": "Bluetooth",
+                "battery_life": "12 months"
+            },
+            "ratings": {
+                "average": 4.5,
+                "count": 240
+            }
+        },
+        {
+            "id": "P1002",
+            "name": "Mechanical Keyboard",
+            "brand": "KeyChron",
+            "price": 79.99,
+            "currency": "USD",
+            "stock": 57,
+            "specs": {
+                "color": "White",
+                "switch_type": "Gateron Brown",
+                "backlight": "RGB"
+            },
+            "ratings": {
+                "average": 4.7,
+                "count": 145
+            }
+        },
+        {
+            "id": "P1003",
+            "name": "Sony Wireless Noise-Canceling Headphones",
+            "brand": "Sony",
+            "price": 199.99,
+            "currency": "USD",
+            "stock": 23,
+            "specs": {
+                "color": "Black",
+                "connectivity": "Bluetooth",
+                "noise_cancellation": "Active",
+                "battery_life": "30 hours"
+            },
+            "ratings": {
+                "average": 4.8,
+                "count": 529
+            }
+        }
+    ]
+}
+
+brand_list = []
+for i in product_data["products"]:
+    if i["brand"] not in brand_list:
+        brand_list.append(i["brand"])
+print(brand_list)
